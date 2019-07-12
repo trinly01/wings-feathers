9ce22555526a3e7649a2a7c00f8457b4c55d4c4c
Trinmar Boado <timi.boado@gmail.com>
2019-07-12 13:25:58 +0800

    "version": "4.0.0"
    opensourced

---
+++ README.md
@@ -0,0 +1,172 @@
+# Welcome to Wings-Feathers!
+
+<p align="center">
+  <img width="300" src="https://miro.medium.com/max/3728/1*7zccGWE4o5LmxegijjK_xQ.png"/>
+  <br />
+  <img width="300" src="https://feathersjs.com/img/feathers-logo-wide.png" />
+</p>
+
+A **FeathersJS 4-Way reactive data sync** for any frontend framework
+
+ - [x] DOM / UI (HTML)
+ - [x] Data / State (Javascript)
+ - [x] Local Storage (Offline)
+ - [x] Backend/Database (Cloud)
+
+> **Join and support our Community** <br />
+> Web and Mobile Developers PH <br />
+> [ [Facebook Page](https://fb.com/webmobile.ph) | [Group](https://fb.com/groups/webmobile.ph/) ]
+
+## Installation
+
+```bash
+npm install wings-feathers
+```
+or
+```bash
+yarn add wings-feathers
+```
+
+## Usage
+
+```javascript
+import  wings  from  'wings-feathers'
+
+let app = wings('http://localhost:3030')
+
+let messagesSrvc = app.wingsService('messages')
+
+messageSrvc.on('dataChange', (messages) => {
+	console.log(messages)
+})
+
+messageSrvc.init()
+```
+## app.wingsService(serviceName, params, config)
+Returns a wingsService `<object>`
+
+| Param| Type | Description |
+|--|--|--|
+| *serviceName* | `<string>` | Name of service |
+| *params.query* | `<object>` | ( Optional ) Refer to  [Feathers Querying](https://docs.feathersjs.com/api/databases/querying) |
+| *config* | `<object>` | ( Optional ) Configuration of wingsService `<object>` |
+| *config.channels* | `<array>` | ( Optional ) Array of [channel](#channel) objects |
+
+<span id="params"></span>
+### *params.query* `<object>` 
+Refer to [Feathers Querying](https://docs.feathersjs.com/api/databases/querying)
+```javascript
+/* example records
+  [
+    { text: 'Hello', read: true, roomId: 1, nested: { prop: 'xander' } },
+    { text: 'World', read: false, roomId: 2, nested: { prop: 'ford' } }
+  ]
+*/
+
+let serviceName = 'message'
+
+let params = {
+  query: {
+    read: false,
+    roomId: 2
+  }
+}
+
+let messagesSrvc = app.wingsService(serviceName, params)
+
+messageSrvc.on('dataChange', (messages) => {
+  console.log(messages)
+  // [ { text: 'World', read: false, roomId: 2, nested: { prop: 'ford' } } ]
+})
+```
+
+<span id="config"></span>
+### *config* `<object>` 
+| Property | Type | Default | Description |
+|--|--|--|--|
+| *debug* | `<boolean>` | false | Logs all events `init`, `created`, `removed`, `patched`, `updated`, `loadMore`, `reset`  |
+| *newDataPosition* | `<string>` | 'end' | Add new items to the `start` or `end` of an array  |
+| *paginate* | `<boolean>` | false | Enable pagination based on `$limit`.  ***default is 10 records per page* |
+| *channels* | `<array>` | [] | Refer to [channels](#channel) |
+
+```javascript
+let config = {
+  debug: true,
+  newDataPosition: 'start',
+  paginate: true,
+  channels: []
+} 
+
+let messagesSrvc = app.wingsService(serviceName, params, config)
+```
+
+<span id="channel"></span>
+### *config.channels* `<array>` and *channel* `<object>`
+Channels determine which records to receive that passes the prop === value .
+
+| Property | Type | Description |
+|--|--|--|
+| *prop* | `<string>` | Name of record's property |
+| *value* | `<string | number | boolean | function>` | Equality test value |
+| *value* | `<function>` | `callback` accepts `(val, message)` arguments for custom test. ***Must return a boolean value* |
+
+```javascript
+/* example records
+  [
+    { text: 'Hello', read: true, roomId: 1, nested: { prop: 'xander' } },
+    { text: 'World', read: false, roomId: 2, nested: { prop: 'ford' } }
+  ]
+*/
+
+let config = {
+  channels: [
+    { prop: 'roomId', value: 2},
+    { prop: 'nested.prop', value: 'ford'},
+    { prop: 'nested', value: (val) => val.prop === 'ford' }
+  ]
+}
+
+let messagesSrvc = app.wingsService(serviceName, params, config)
+```
+
+> You may use dot notation in **prop** as reference into the object's property
+
+## reset(params, config)
+set new `params`, `config` and triggers `init` method
+```javascript
+let params = {
+  query: {
+    read: false,
+    roomId: 2
+  }
+}
+
+let config = {
+  debug: true,
+  newDataPosition: 'start',
+  paginate: true,
+  channels: []
+}
+
+messagesSrvc.reset(params, config)
+``` 
+
+## loadMore()
+loads more data based on `$skip` = ( `page` + 1 ) * `$limit`  
+```javascript
+messagesSrvc.loadMore()
+``` 
+
+## loadAll()
+loads all data based on (`$skip` = `page` * `$limit`) * `pages`
+```javascript
+messagesSrvc.loadAll()
+``` 
+
+## loadPage
+loads the based on  `$skip` = `page` * `$limit`
+```javascript
+messagesSrvc.loadPage(2)
+```
+
+# *Join and support our Community* <br /> **Web and Mobile Developers PH** <br/> [ [Facebook Page](https://fb.com/webmobile.ph) | [Group](https://fb.com/groups/webmobile.ph/) ]
---
+++ index.js
@@ -0,0 +1,259 @@
+import io from 'socket.io-client'
+
+// import feathers from '@feathersjs/feathers'
+// import auth from '@feathersjs/authentication-client'
+// import socketio from '@feathersjs/socketio-client'
+import feathers from '@feathersjs/client/dist/feathers.min.js'
+let auth = feathers.authentication
+let socketio = feathers.socketio
+
+import Event from 'events'
+
+import { findIndex, isFunction, ceil, unionBy } from 'lodash'
+// const stackTrace = require('stack-trace')
+
+export default (host) => {
+  host = host || 'http://localhost:3030' // IP and port of the server
+  let socket = io(host)
+
+  let app = feathers()
+
+  app
+    .configure(socketio(socket, {
+      timeout: 180000
+    }))
+    .configure(auth({
+      jwtStrategy: 'jwt', // the name of the JWT authentication strategy
+      entity: 'user', // the entity you are authenticating (ie. a users)
+      service: 'users', // the service to look up the entity
+      cookie: host + '-jwt', // the name of the cookie to parse the JWT from when cookies are enabled server side
+      storageKey: host + '-jwt', // the key to store the accessToken in localstorage or AsyncStorage on React Native
+      storage: window.localStorage // Passing a WebStorage-compatible object to enable automatic storage on the client.
+    }))
+
+  app.wingsService = (serviceName, query = {}, conf = {}) => {
+    let event = new Event()
+    let service = app.service(serviceName)
+    let wings = {
+      service,
+      query,
+      total: 0,
+      data: [],
+      skip: 0,
+      page: 1,
+      limit: 10,
+      channels: (conf.channels || []),
+      debug: (conf.debug || false),
+      newDataPosition: (conf.newDataPosition || 'end'),
+      paginate: (conf.paginate || false)
+    }
+
+    wings.reset = (query, conf = {}) => {
+      Object.assign(wings, {
+        total: 0,
+        data: [],
+        skip: 0,
+        page: 1,
+        limit: 10
+      })
+      wings.query = (query | wings.query)
+      wings.channels = (conf.channels | wings.channels)
+      wings.debug = (conf.debug | wings.debug)
+      wings.newDataPosition = (conf.newDataPosition | wings.newDataPosition)
+      wings.paginate = (conf.paginate | wings.paginate)
+      wings.log(`${serviceName}.reset`)
+      wings.init()
+    }
+
+    wings.log = function () {
+      let style = `
+        display: inline-block;
+        padding: 0 10px;
+        height: 50px;
+        line-height: 20px;
+        border-radius: 25px;
+        background-color: #FF5722;
+        color: white;
+        font-weight: bold;
+      `
+      let title = `%c${arguments[0]}`
+      Array.prototype.shift.apply(arguments)
+      // const trace = stackTrace.get()
+      // Array.prototype.unshift.call(arguments, `${trace[2].getFileName()}:${trace[2].getLineNumber()}`)
+
+      var err = new Error()
+      // var callerLine = err.stack.split('\n')[4]
+      // var index = callerLine.indexOf('at ')
+      // var clean = callerLine.slice(index + 2, callerLine.length)
+      let errStackSplitAt = err.stack.toString().split('\n')
+      let stack2 = errStackSplitAt[2].split('?!.')
+      let file = stack2[stack2.length - 1].split('?')[0].split('///.')
+      file = file.length === 1 ? file[0] : file[1].split(':')[0]
+      let stack = err.stack.split('\n')
+      stack[0] = 'Error'
+      stack.splice(1, 1)
+      stack.splice(2, 8)
+      err.stack = stack.join('\n')
+
+      // console.log(file)
+
+      if (wings.debug) console.log(title, style, file, err, '\n', ...arguments)
+    }
+
+    wings.on = (evtName, listener) => {
+      event.on(evtName, function () {
+        listener.bind(wings)
+        listener(...arguments, wings)
+      })
+      wings.listener = listener
+      console.log(event)
+      return wings
+    }
+
+    wings.destroy = () => {
+      // event.removeAllListeners('dataChange')
+      delete event._events
+    }
+
+    wings.init = () => {
+      (async () => {
+        try {
+          let result = await service.find(query)
+          wings = Object.assign(wings, result)
+          event.emit('dataChange', result.data)
+          wings.log(`${serviceName}.init`, result)
+          return wings
+        } catch (error) {
+          wings.log(`${serviceName}.init`, error)
+        }
+      })()
+      event.emit('dataChange', wings.data)
+      return wings
+    }
+
+    wings.find = function () { return service.find(...arguments) }
+    wings.get = function () { return service.get(...arguments) }
+    wings.create = function () { return service.create(...arguments) }
+    wings.update = function () { return service.update(...arguments) }
+    wings.patch = function () { return service.patch(...arguments) }
+    wings.remove = function () { return service.remove(...arguments) }
+
+    wings.inChannel = message => {
+      return (
+        !wings.channels[0] || // isEmpty returns inChannel
+        findIndex(wings.channels, ch => {
+          let nestedProp = ch.prop.split('.').reduce((o, i) => o[i], message)
+          if (isFunction(ch.value)) {
+            return ch.value(nestedProp, message)
+          } else {
+            return ch.value === nestedProp
+          }
+        }) !== -1
+      )
+    }
+
+    service.on(`created`, message => {
+      wings.total += 1
+      if (wings.inChannel(message)) {
+        if (wings.newDataPosition === 'end') {
+          wings.data.push(message)
+          if (wings.paginate && wings.data.length > wings.limit) wings.data.shift()
+        } else {
+          wings.data.unshift(message)
+          if (wings.paginate && wings.data.length > wings.limit) wings.data.pop()
+        }
+        event.emit('dataChange', wings.data)
+      }
+    })
+
+    wings.findIndex = message => {
+      var index = -1
+      let len = wings.data.length
+      for (var i = 0; i < len; i += 1) {
+        if (wings.data[i]._id === message._id) {
+          index = i
+          break
+        }
+      }
+      return index
+    }
+
+    wings.removeItem = (index) => {
+      var stop = wings.data.length - 1
+      while (index < stop) {
+        wings.data[index] = wings.data[++index]
+      }
+      wings.data.pop()
+    }
+
+    service.on(`removed`, message => {
+      wings.total -= 1
+      let index = wings.findIndex(message)
+      if (index !== -1) {
+        wings.removeItem(index)
+        if (wings.data.length < wings.limit) wings.loadPage(1)
+        wings.log(`${serviceName}.removed`, message, index)
+        event.emit('dataChange', Array.from(wings.data))
+      }
+    })
+
+    wings.modifyData = (message, type = 'patched') => {
+      let index = wings.findIndex(message)
+      let inChannel = wings.inChannel(message)
+      if (index !== -1 && inChannel) { // in data and channel
+        wings.data[index] = message
+      } else if (index !== -1 && !inChannel) { // in data but not in channel
+        wings.removeItem(index)
+      } else if (index === -1 && inChannel && !wings.paginate) { // Not in data but in channel
+        if (wings.newDataPosition === 'end') wings.data.push(message); else wings.data.unshift(message)
+      }
+      event.emit('dataChange', Array.from(wings.data))
+      wings.log(`${serviceName}.on.${type}`, message, 'index', index, 'inChannel', inChannel)
+    }
+
+    service.on(`patched`, message => {
+      wings.modifyData(message)
+    })
+
+    service.on(`updated`, message => {
+      wings.modifyData(message, 'updated')
+    })
+
+    wings.loadMore = async () => {
+      let pages = ceil(wings.total / wings.limit)
+      if (wings.page <= pages) {
+        query.query.$skip = wings.page * wings.limit
+        let result = await service.find(query)
+        ++wings.page
+        wings.data = unionBy(wings.data, result.data, '_id')
+        event.emit('dataChange', wings.data)
+        wings.log(`${serviceName}.loadMore`, result)
+      }
+    }
+
+    wings.loadAll = async () => {
+      let pages = ceil(wings.total / wings.limit)
+      if (wings.page !== pages) {
+        await wings.loadMore()
+        service.loadAll()
+      }
+    }
+
+    wings.loadPage = async (p = 1) => {
+      // let pages = ceil(wings.total / wings.limit)
+      wings.page = p
+      // if (wings.page) {
+      query.query.$skip = (wings.page - 1) * wings.limit
+      let result = await service.find(query)
+      Object.assign(wings, result)
+      event.emit('dataChange', wings.data)
+      wings.log(`${serviceName}.loadPage`, result)
+      // }
+    }
+
+    // wings.init()
+    return wings
+  }
+
+  return app
+}
